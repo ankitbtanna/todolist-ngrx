e4c18e085051d109a80d377f8b20722e51a6558a
Vinutha Govindaraju <vinu.raju@gmail.com>
2023-02-12 01:28:48 +0530

    create apps

---
+++ apps/todolist-ngrx-api/jest.config.ts
@@ -0,0 +1,16 @@
+/* eslint-disable */
+export default {
+  displayName: 'todolist-ngrx-api',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/todolist-ngrx-api',
+};
