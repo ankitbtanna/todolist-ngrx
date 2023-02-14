91b78a1c1dd0f2f35b50b826659a6d3601d11e68
Vinutha Govindaraju <vinu.raju@gmail.com>
2023-02-14 23:23:43 +0530

    add form

--- apps/todolist-ngrx/src/app/app.module.ts
+++ apps/todolist-ngrx/src/app/app.module.ts
@@ -1,11 +1,20 @@
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import { MaterialModule } from './material.module';
+import { NgModule } from '@angular/core';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MaterialModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
